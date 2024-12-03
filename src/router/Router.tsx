f91d6f3c484713617bc7bc47c010b34eb7eefdd9
21120079 <hungtruonghl11@gmail.com>
2024-12-04 01:25:36 +0700

    update

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { paths } from "./path"
 import PrivateRoute from './PrivateRoute';
 import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+import Login from "../pages/SignIn";
+import Register from "../pages/SignUp";
 import Private from "../pages/Private";
 
 export default function AppRouter() {
