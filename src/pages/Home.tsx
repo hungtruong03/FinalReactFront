33522688a9df6d2d8a551386c971ca790453e642
21120079 <hungtruonghl11@gmail.com>
2024-12-04 18:39:38 +0700

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,19 @@ const Home: React.FC = () => {
     const message = location.state?.message;
 
     return (
-        <>
+        <div className="bg-[url('https://i.pinimg.com/originals/07/21/1b/07211b078ab5e9f537a3daeccef72279.jpg')] bg-cover bg-center bg-no-repeat min-h-screen flex flex-col text-white">
             <Navbar />
-            <div className="text-center mt-5">
-                <h1>Welcome to the Homepage</h1>
-                {message && <p className="alert alert-info">{message}</p>}
+            <div className="flex-grow flex items-center justify-center">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold">Welcome to the Homepage</h1>
+                    {message && (
+                        <p className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg inline-block">
+                            {message}
+                        </p>
+                    )}
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
--- src/pages/Private.tsx
+++ src/pages/Private.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
-const Private: React.FC = () => {
+const Home: React.FC = () => {
+    const location = useLocation();
+    const message = location.state?.message;
+
     return (
-        <>
+        <div className="bg-[url('https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/hinh-nen-may-tinh-dep-a-41-1.jpg')] bg-cover bg-center bg-no-repeat min-h-screen flex flex-col text-white">
             <Navbar />
-            <div className="text-center mt-5">
-                <h1>This is the private page</h1>
+            <div className="flex-grow flex items-center justify-center">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold">This is the private page</h1>
+                    {message && (
+                        <p className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg inline-block">
+                            {message}
+                        </p>
+                    )}
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
-export default Private;
+export default Home;
